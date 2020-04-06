d7900df41a829f5cd1f9f17e5e45be58b54408f4
Samistar <denis.itunin@mail.ru>
2020-04-06 23:17:06 +0300

    фёрсткаммит

--- src/main/java/ru/samistar/bot/BotApplication.java
+++ src/main/java/ru/samistar/bot/BotApplication.java
@@ -1,71 +1,21 @@
 package ru.samistar.bot;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
-import ru.samistar.bot.modal.Bank;
-import ru.samistar.bot.telegram.Bot;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-@SpringBootApplication
-public class BotApplication {
+@SpringBootApplication // анатация(метка) которой помечается главный класс спринга
+@EnableScheduling // включает шидулеры(хрень которая вызывает повторение выполнения метода раз в указанное количество времени)
+public class BotApplication{
+
 
 	public static void main(String[] args) throws IOException {
-		SpringApplication.run(BotApplication.class, args);
-		fill();
-		banks.forEach(System.out::println);
+        ApiContextInitializer.init();
 
-		System.getProperties().put( "proxySet", "true" );
-		System.getProperties().put( "socksProxyHost", "127.0.0.1" );
-		System.getProperties().put( "socksProxyPort", "9150" );
-		ApiContextInitializer.init();
+        SpringApplication.run(BotApplication.class, args); // старт спринг приложения
 
-		TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-		try {
-			telegramBotsApi.registerBot(new Bot());
-		} catch (TelegramApiRequestException e) {
-			e.printStackTrace();
-		}
-
-	}
-	static List<Bank> banks = new ArrayList<>();
-
-	//    fixedDelay = 10_000
-	public static void fill() throws IOException {
-		banks.clear();
-
-		Document document = Jsoup.connect("https://ru.myfin.by/currency/moskva").get();
-
-		Elements elements = document.getElementsByClass("tr-turn");
-
-		System.out.println("elements = " + elements);
-
-		for (int i = 0; i < elements.size(); i++) {
-			String bankName = elements.get(i).getElementsByClass("bank_link").first().text();
-			String dollarBuy = elements.get(i).getElementsByClass("USD").first().text();
-			String dollarSell = elements.get(i).getElementsByClass("USD").last().text();
-			String euroBuy = elements.get(i).getElementsByClass("EUR").first().text();
-			String euroSell = elements.get(i).getElementsByClass("EUR").last().text();
-// пока забить на дату String date = elements.get(i).getElementsByClass("curr_hid").last().text();
-
-//TODO логика заплнения банков и курсов валют
-//Double kurs = Double.valueOf(euroBuy);
-//
-
-
-			Bank bank = new Bank(bankName, Double.valueOf(dollarSell), Double.valueOf(dollarBuy), Double.valueOf(euroSell), Double.valueOf(euroBuy));
-			banks.add(bank);
-			//System.out.println(bankName + " " + dollarBuy + " " + dollarSell + " " + euroBuy + " " + euroSell + " " + date);
-		}
-	}
-
-
+    }
 }
--- src/main/java/ru/samistar/bot/telegram/Bot.java
+++ src/main/java/ru/samistar/bot/telegram/Bot.java
@@ -1,11 +1,41 @@
 package ru.samistar.bot.telegram;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component // штука засовывающая класс в хранилище бинов
 public class Bot extends TelegramLongPollingBot {
+
+    @Autowired  // найти в хранилище бинов(классов работников) переменную того же типа что и данное поле
+    private FillBankScheduler fillBankScheduler; // данное поле(ничего само по себе не делает)
+
+    @PostConstruct
+    private void init(){
+        System.getProperties().put( "proxySet", "true" );
+        System.getProperties().put( "socksProxyHost", "127.0.0.1" );
+        System.getProperties().put( "socksProxyPort", "9150" );
+
+
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(this);
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Метод для приема сообщений.
@@ -14,6 +44,9 @@ public class Bot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         String message = update.getMessage().getText();
+        if(message.equals("Курс Валюты")){
+            message = fillBankScheduler.getBanks().get(0).toString();
+        }
         sendMsg(update.getMessage().getChatId().toString(), message);
     }
 
@@ -27,6 +60,7 @@ public class Bot extends TelegramLongPollingBot {
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(chatId);
         sendMessage.setText(s);
+        setButtons(sendMessage);
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
@@ -50,5 +84,48 @@ public class Bot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return "1120734227:AAFMoWfTTbEwYyoEbIm8cvY4lb_AeXC55BY";
+    }
+
+
+    public synchronized void setButtons(SendMessage sendMessage) {
+        // Создаем клавиуатуру
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        // Создаем список строк клавиатуры
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        // Первая строчка клавиатуры
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        // Добавляем кнопки в первую строчку клавиатуры
+        keyboardFirstRow.add(new KeyboardButton("Курс Валюты"));
+
+        // Вторая строчка клавиатуры
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        // Добавляем кнопки во вторую строчку клавиатуры
+        keyboardSecondRow.add(new KeyboardButton("Банки"));
+
+        // Третья строчка клавиатуры
+        KeyboardRow keyboardThirdRow = new KeyboardRow();
+        // Добавляем кнопки в третью строчку клавиатуры
+        keyboardFirstRow.add(new KeyboardButton("Обменники"));
+
+        // Четвёртая строчка клавиатуры
+        KeyboardRow keyboardFourthRow = new KeyboardRow();
+        // Добавляем кнопки в четвёртую строчку клавиатуры
+        keyboardFourthRow.add(new KeyboardButton("Настройки"));
+
+        // Добавляем все строчки клавиатуры в список
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        keyboard.add(keyboardThirdRow);
+        keyboard.add(keyboardFourthRow);
+        // и устанваливаем этот список нашей клавиатуре
+        replyKeyboardMarkup.setKeyboard(keyboard);
+
+
     }
 }
